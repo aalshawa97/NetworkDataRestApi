8b2dce8f8cb3faa8033ddd8dbbfe13cd2da78d28
aalshawa97 <abdullahalshawa@gmail.com>
2022-08-30 19:45:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NetworkDataRestApi
+ Weather
---
+++ app/src/main/java/com/example/networkdatarestapi/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.networkdatarestapi;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.networkdatarestapi.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    //Define all the components that are in the XML file
+    Button btn_getCityId, btn_getWeatherById, btn_getWeatherByName;
+    EditText editText;
+    ListView lv_forecastList;
+
+    //This is the data that will be shown in the listview component.
+    List<WeatherReportModel> forecaseList = new ArrayList<>();
+
+    //Default value for city is not provided by the user.
+    String cityName = "Phoenix";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Assign variables and values to all of the items in the XML layout file.
+        btn_getCityId = findViewById(R.id.btn_getCityId);
+        btn_getWeatherById = findViewById(R.id.btn_useCityId);
+        btn_getWeatherByName = findViewById(R.id.btn_useCityName);
+        //setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        /*
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
